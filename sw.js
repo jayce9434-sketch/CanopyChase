42cd2affdab0749baaf5b3b94f99327b1c9f3d90
jayce9434-sketch <jayce9434@gmail.com>
2026-09-20 14:07:12 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='canopy-chase-v2';
-const ASSETS=['./','./index.html','./style.css','./game.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r;}).catch(()=>caches.match(e.request)))});
